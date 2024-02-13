2f5680f57f14233e28fe8576f23c280ee52a9c08
smbanati-IZxCKm <smbanati@gmail.com>
2024-02-13 18:56:40 -0500

    set up project folder, and made schema

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS employeeTracker_db;
+CREATE DATABASE employeeTracker_db;
+USE employeeTracker_db;
+
+
+CREATE TABLE departments (
+    department_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(300) NOT NULL
+);
+
+CREATE TABLE roles (
+    roles_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(300),
+    salary DECIMAL(10, 2) -- Stores 10 digitgs with 2 digits to the right of the decimal point 
+    department_id INT, -- References department by 
+    FOREIGN KEY (department_id) REFERENCES departments(deparment_id)
+    REFERENCES departments(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employees (
+    employee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL, 
+    role_id INT,
+    manager_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES roles(roles_id),
+    FOREIGN KEY (manager_id) REFERENCES employees(employee_id)
+);
+
+
+
+
