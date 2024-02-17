59140178d4b5cd1ad3e3959003bfa4cd453188d2
smbanati-IZxCKm <smbanati@gmail.com>
2024-02-17 15:14:00 -0500

    added cfonts function to make a pretty title, and added start inquirer function for intro prompts

--- server.js
+++ server.js
@@ -24,3 +24,48 @@ db.connect((err) => {
     
     start();
 })
+
+// Starts the cfont title 
+cfonts.say('FullStack Employee Manager ', {
+	font: 'block',              // define the font face
+	align: 'left',              // define text alignment
+	colors: ['candy'],         // define all colors
+	background: 'transparent',  // define the background color, you can also use `backgroundColor` here as key
+	letterSpacing: 1,           // define letter spacing
+	lineHeight: 1,              // define the line height
+	space: true,                // define if the output text should have empty lines on top and on the bottom
+	maxLength: '0',             // define how many character can be on one line
+	gradient: false,            // define your two gradient colors
+	independentGradient: false, // define if you want to recalculate the gradient for each new line
+	transitionGradient: false,  // define if this is a transition between colors directly
+	env: 'node'                 // define the environment cfonts is being executed in
+});
+
+
+// Function to Start Thomas SQL Employee Tracker Application
+function start() {
+    inquirer
+        .prompt({
+            type: "list",
+            name: "action",
+            message: "What would you like to do?",
+            choices: [
+                "View All Departments",
+                "View All Roles",
+                "View All Employees",
+                "Add A Department",
+                "Add A Role",
+                "Add An Employee",
+                "Update An Employee Role",
+                'View Employees By Manager',
+                'View Employees By Department',
+                'Update Employee Managers',
+                'Delete Departments, roles, and employees',
+                "Quit",
+            ],
+        })
+        .then((answer) => {
+            
+
+        })
+    };
